623ec7403ad74e90669b4d8c5ec3fc82f0b902d0
Alejandro Fuentes <alejandrodfco@gmail.com>
2023-09-15 11:59:23 +0200

    Added a modal to display menu options

--- src/components/Oferta/Oferta.js
+++ src/components/Oferta/Oferta.js
@@ -1,14 +1,34 @@
 import React, { useState } from "react";
 import carta from "./images/Carta.jpg";
-import Bodega from "./images/Bodega.pdf"; 
+import Bodega from "./images/Bodega.pdf";
 import Comida from "./images/Comida.pdf";
 
 const Oferta = ({ lenguage }) => {
   const [menuOpen, setMenuOpen] = useState(false);
+
   const handleMenu = () => {
-    const menu = document.getElementById("menu-description");
-    menu.classList.toggle("menu-description-open");
+    const menuSection = document.getElementById("menu-section");
+
+    if (menuSection) {
+      const menuSectionRect = menuSection.getBoundingClientRect();
+      const modal = document.getElementById("modal");
+      const modalContainer  = document.getElementById("modal-container");
+
+
+      if (modal) {
+        modalContainer.style.width = `${menuSectionRect.width}px`;
+
+        modal.style.top = `${menuSectionRect.top}px`; // Position above the menu-section
+        modal.style.left = `${menuSectionRect.left-200}px`; // Adjust as needed
+        modal.style.right = `${menuSectionRect.right}px`; // Adjust as needed
+        modal.style.bottom = `${menuSectionRect.bottom}px`; // Adjust as needed
+      }
+    }
     setMenuOpen(!menuOpen);
+  };
+
+  const closeMenuModal = () => {
+    setMenuOpen(false);
   };
 
   if (lenguage === "EN") {
@@ -31,30 +51,27 @@ const Oferta = ({ lenguage }) => {
                 childhood, reflected in a timeless menu proposal.
               </p>
               <div className="offer-letter-buttons">
-                <button><a href={Bodega} target="_blank" >Wine Cellar</a></button>
-                <button><a href={Comida} target="_blank"></a>View the Menu</button>
+                <button>
+                  <a href={Bodega} target="_blank">
+                    Wine Cellar
+                  </a>
+                </button>
+                <button>
+                  <a href={Comida} target="_blank"></a>View the Menu
+                </button>
               </div>
             </div>
             <div className="offer-menu">
-              <div className="offer-title">
-                <h3>The Menu</h3>
-                <p>Based on seasonal products</p>
-              </div>
-              <ul
-                className={`${
-                  menuOpen ? "menu-description-open" : "offer-menu-description"
-                }`}
-                id="menu-description"
-              >
-                <li>Includes:</li>
-                <li>2 Starters</li>
-                <li>Fish, meat, and dessert</li>
-                <li>Water and bread service</li>
-                <li>37 euros per person</li>
-              </ul>
-
+              <div className="menu-img"></div>
+              <h3>The Menu</h3>
+              <p>
+                It's the result of days spent shopping at the market,
+                cultivating the land after generations of family heritage, using
+                local ingredients, and following seasonal availability. All of
+                this is showcased in a menu that changes with the seasons.
+              </p>
               <div className="offer-letter-buttons menu-button">
-                <button onClick={handleMenu}>View the Menu</button>
+                <button>View the Menu</button>
               </div>
             </div>
           </div>
@@ -82,29 +99,46 @@ const Oferta = ({ lenguage }) => {
               propuesta atemporal.
             </p>
             <div className="offer-letter-buttons">
-              <button><a href={Bodega} target="_blank">Ver La Bodega</a></button>
-              <button><a href={Comida} target="_blank">Ver La Carta</a></button>
+              <button>
+                <a href={Bodega} target="_blank">
+                  Ver La Bodega
+                </a>
+              </button>
+              <button>
+                <a href={Comida} target="_blank">
+                  Ver La Carta
+                </a>
+              </button>
             </div>
           </div>
-          <div className="offer-menu">
-            <div className="offer-title">
-              <h3>El menú</h3>
-              <p>Según producto de temporada</p>
-            </div>
-            <ul
-              className={`${
-                menuOpen ? "menu-description-open" : "offer-menu-description "
-              }`}
-              id="menu-description"
-            >
-              <li>Incluye:</li>
-              <li>2 Entrantes</li>
-              <li>Pescado, carne y postre</li>
-              <li>Servicio de agua y pan</li>
-              <li>37 euros por persona</li>
-            </ul>
+          <div className="offer-menu" id="menu-section">
+            <div className="menu-img"></div>
+            <h3>El menú</h3>
+            <p>
+              Es el resultado de los días de compra en el mercado, el cultivo de
+              la tierra tras la herencia familiar, el kilometro cero y la
+              temporalidad, plasmadas en un menú que varía en consonancia con la
+              época del año
+            </p>
             <div className="offer-letter-buttons menu-button">
-              <button onClick={handleMenu}>Ver el menú</button>
+              <button id="menu-section" onClick={handleMenu}>Ver el menú</button>
+            </div>
+            <div className={`${menuOpen ? "modal-open" : "modal"}`} id="modal-container">
+              <div className="modal-content" id="modal">
+                <div className="modal-content-title">
+                  <h4>Incluye:</h4>
+                  <button className="modal-button"onClick={closeMenuModal}>Ø</button>
+                </div>
+                <div className="modal-text">
+                  <ul>
+                    <li>2 Entrantes</li>
+                    <li>Pescado, carne y postre</li>
+                    <li>Servicio de agua y pan</li>
+                    <li>37 euros por persona</li>
+                  </ul>
+                </div>
+                <p>El menú se elabora según los productos del mercado</p>
+              </div>
             </div>
           </div>
         </div>
