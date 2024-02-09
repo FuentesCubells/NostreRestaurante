251626dea61fa4e05ae2769c1ee4d819c4a6d27b
FuentesCubells <alejandrodfco@gmail.com>
2024-02-09 19:12:47 +0100

    Added new buttons and photos

--- src/components/About/About.js
+++ src/components/About/About.js
@@ -1,5 +1,7 @@
 import React from "react";
-import us from "./images/Somos.jpg";
+import Andres from "./images/andres.jpg"
+import Alberto from "./images/alberto.jpg"
+import Nadia from "./images/Nadia.jpg"
 
 const About = ({ lenguage }) => {
   if (lenguage === "EN") {
@@ -14,61 +16,66 @@ const About = ({ lenguage }) => {
             </p>
           </div>
           <div className="about-img">
-            <figure>
-              <img
-                src={us}
-                alt="Andrés Rengel, Nadia Abramova, and Pamela Martinez"
-              />
-            </figure>
             <div className="about-content">
               <div className="about">
-                <h3>Andrés Rengel:</h3>
-                <p>
-                  A Valencian with Andalusian, Castilian, and Aragonese roots.
-                  After studying cooking, he decided to work in various
-                  Michelin-starred restaurants like La Finca, El Celler de Can
-                  Roca, and Arrea to perfect his skills. He eventually entered
-                  the teaching field, instructing vocational training classes.
-                  However, since he began his culinary journey, he had a dream
-                  of opening his own restaurant. Nostre is his most daring and
-                  personal venture, and he is a true gastronomy enthusiast.
-                  Among his hobbies, you'll find him preparing for cooking
-                  competitions, enjoying the mountains, and secretly being quite
-                  the dancer.
-                </p>
+              <figure><img src={Andres}/></figure>
+                <div className="about-data">
+                  <h3>Andrés Rengel:</h3>
+                  <p>
+                    A Valencian with Andalusian, Castilian, and Aragonese roots.
+                    After studying cooking, he decided to work in various
+                    Michelin-starred restaurants like La Finca, El Celler de Can
+                    Roca, and Arrea to perfect his skills. He eventually entered
+                    the teaching field, instructing vocational training classes.
+                    However, since he began his culinary journey, he had a dream
+                    of opening his own restaurant. Nostre is his most daring and
+                    personal venture, and he is a true gastronomy enthusiast.
+                    Among his hobbies, you'll find him preparing for cooking
+                    competitions, enjoying the mountains, and secretly being quite
+                    the dancer.
+                  </p>
+                </div>
               </div>
               <div className="about">
-                <h3>Nadia Abramova:</h3>
-                <p>
-                  A programming graduate, Nadia started her culinary studies in
-                  Russia but was drawn to Mediterranean cuisine and its
-                  possibilities. She decided to come to Valencia to further her
-                  culinary education. An avid nature lover, she spends a
-                  significant part of her free time gathering her own herbs and
-                  has become an expert in fermentations. You must definitely try
-                  her lemonades, and if you're lucky, some of her homemade
-                  kombuchas!
-                </p>
+              <figure><img src={Nadia}/></figure>
+                <div className="about-data">
+                  <h3>Nadia Abramova:</h3>
+                  <p>
+                    A programming graduate, Nadia started her culinary studies in
+                    Russia but was drawn to Mediterranean cuisine and its
+                    possibilities. She decided to come to Valencia to further her
+                    culinary education. An avid nature lover, she spends a
+                    significant part of her free time gathering her own herbs and
+                    has become an expert in fermentations. You must definitely try
+                    her lemonades, and if you're lucky, some of her homemade
+                    kombuchas!
+                  </p>
+                </div>
+                
               </div>
               <div className="about">
-                <h3>Pamela Martínez:</h3>
-                <p>
-                  Born in Colombia but with German roots, Pamela went to Germany
-                  to study cooking at the age of 18. After gaining years of
-                  experience, Pamela decided to step out of the kitchen and
-                  specialize in front-of-house service. She's passionate about
-                  rock climbing, but above all, she loves traveling. She ended
-                  up in Valencia a year ago and fell in love with the city,
-                  making it her home. As a seasoned traveler, Pamela will guide
-                  you throughout the service to ensure you have an unforgettable
-                  experience.
-                </p>
+              <figure><img src={Alberto}/></figure>
+                <div className="about-data">
+                  <h3>Alberto Pastor:</h3>
+                  <p>
+                    Our comments on the
+                    gastronomy dates back to 2003. He studied Hospitality in
+                    Murcia has worked in multiple fine dining restaurants
+                    in Valencia, Elche and Murcia. It's a real whirlwind as if
+                    Your best friend is the driver so you can spend a little more
+                    power chargers. If you receive it with a sound,
+                    You will guide throughout the service to live an experience.
+                    unforgettable and you will also have the security that you will be able to carry your card
+                    the occasional laugh.
+                  </p>
+                </div>
+                
               </div>
             </div>
           </div>
         </div>
       </div>
-    )
+    );
   }
   return (
     <div className="about-fullContainer">
@@ -78,50 +85,60 @@ const About = ({ lenguage }) => {
           <p>Somos el sentimiento de quienes cuando cocinan, se comprometen.</p>
         </div>
         <div className="about-img">
-          <figure>
-           
-          </figure>
           <div className="about-content">
             <div className="about">
-              <h3>Andrés Rengel:</h3>
-              <p>
-                Valenciano de raíces andaluzas, castellanas y aragonesas. Tras
-                estudiar cocina, decide pasar por diferentes restaurantes de
-                estrella Michelin, como La Finca, El Celler de Can Roca, Arrea,
-                para perfeccionar sus conocimientos. Termina en la docencia,
-                dando clases de formación profesional, pero desde que se inició
-                en la cocina, había tenido el sueño de abrir su propio
-                restaurante. Nostre es la apuesta más arriesgada y personal de
-                este adicto a la gastronomía. Entre sus hobbies, está el
-                prepararse para ganar concursos de cocina, la montaña y aunque
-                lo niegue, ser todo un bailarín.
-              </p>
+              <figure><img src={Andres}/></figure>
+              <div className="about-data">
+                <h3>Andrés Rengel:</h3>
+                <p>
+                  Valenciano de raíces andaluzas, castellanas y aragonesas. Tras
+                  estudiar cocina, decide pasar por diferentes restaurantes de
+                  estrella Michelin, como La Finca, El Celler de Can Roca, Arrea,
+                  para perfeccionar sus conocimientos. Termina en la docencia,
+                  dando clases de formación profesional, pero desde que se inició
+                  en la cocina, había tenido el sueño de abrir su propio
+                  restaurante. Nostre es la apuesta más arriesgada y personal de
+                  este adicto a la gastronomía. Entre sus hobbies, está el
+                  prepararse para ganar concursos de cocina, la montaña y aunque
+                  lo niegue, ser todo un bailarín.
+                </p>
+              </div>
+              
             </div>
             <div className="about">
-              <h3>Nadia Abramova:</h3>
-              <p>
-                Licenciada en programación, Nadia inicio sus estudios de cocina
-                en Rusia, pero movida por la cocina mediterránea y todas sus
-                posibilidades pronto decidió venir a formarse en gastronomía a
-                Valencia. Fan incondicional de la naturaleza, pasa gran parte de
-                su tiempo libre recolectando sus propias hierbas y se ha
-                convertido en toda una experta de las fermentaciones. Sin duda,
-                tenéis que probar sus limonadas y si sois afortunados ¡alguna de
-                sus combuchas caseras!
-              </p>
+              <figure><img src={Nadia}/></figure>
+              <div className="about-data">
+                <h3>Nadia Abramova:</h3>
+                <p>
+                  Licenciada en programación, Nadia inicio sus estudios de cocina
+                  en Rusia, pero movida por la cocina mediterránea y todas sus
+                  posibilidades pronto decidió venir a formarse en gastronomía a
+                  Valencia. Fan incondicional de la naturaleza, pasa gran parte de
+                  su tiempo libre recolectando sus propias hierbas y se ha
+                  convertido en toda una experta de las fermentaciones. Sin duda,
+                  tenéis que probar sus limonadas y si sois afortunados ¡alguna de
+                  sus combuchas caseras!
+                </p>
+              </div>
+              
             </div>
             <div className="about">
-              <h3>Pamela Martínez:</h3>
-              <p>
-                Colombiana de nacimiento pero con raíces alemanas, a los 18
-                años, se va a estudiar cocina a Alemania. Tras años de
-                experiencia, Pamela decide dar un paso y salir de la cocina para
-                especializarse en sala. Enamorada de la escalada, pero sobre
-                todo de los viajes, termina visitando Valencia, donde se enamora
-                de la ciudad y decide convertirla en su residencia, ya hace un
-                año. Como buena viajera, Pamela os guiará durante todo el
-                servicio para que viváis una experiencia inolvidable.
-              </p>
+              <figure><img src={Alberto}/></figure>
+              <div className="about-data">
+                <h3>Alberto Pastor:</h3>
+                <p>
+                  Sus comienzos en la
+                  gastronomía se remontan al año 2003. Estudió Hostelería en
+                  Murcia y ha trabajado en múltiples restaurantes de alta cocina
+                  en Valencia, Elche y Murcia. Es un auténtico torbellino así que
+                  su mejor aliado es el deporte para poder agotar un poco sus
+                  cargadores de energía. Siempre te recibirá con una sonrisa, te
+                  guiará durante todo el servicio para que vivas una experiencia
+                  inolvidable y además te aseguramos que será capaz de sacarte
+                  alguna que otra carcajada.
+                </p>
+              </div>
+              
             </div>
           </div>
         </div>
--- src/components/Oferta/Oferta.js
+++ src/components/Oferta/Oferta.js
@@ -1,5 +1,6 @@
 import React, { useState } from "react";
 import carta from "./images/Carta.jpg";
+import menu from "./images/menu.jpg";
 import Bodega from "./images/Bodega.pdf";
 import Comida from "./images/Comida.pdf";
 
@@ -12,14 +13,13 @@ const Oferta = ({ lenguage }) => {
     if (menuSection) {
       const menuSectionRect = menuSection.getBoundingClientRect();
       const modal = document.getElementById("modal");
-      const modalContainer  = document.getElementById("modal-container");
-
+      const modalContainer = document.getElementById("modal-container");
 
       if (modal) {
         modalContainer.style.width = `${menuSectionRect.width}px`;
 
         modal.style.top = `${menuSectionRect.top}px`; // Position above the menu-section
-        modal.style.left = `${menuSectionRect.left-200}px`; // Adjust as needed
+        modal.style.left = `${menuSectionRect.left - 200}px`; // Adjust as needed
         modal.style.right = `${menuSectionRect.right}px`; // Adjust as needed
         modal.style.bottom = `${menuSectionRect.bottom}px`; // Adjust as needed
       }
@@ -59,20 +59,85 @@ const Oferta = ({ lenguage }) => {
                 <button>
                   <a href={Comida} target="_blank"></a>View the Menu
                 </button>
+                <button>
+                  <a href="/#reservation">Make a reservation</a>
+                </button>
               </div>
             </div>
-            <div className="offer-menu">
-              <div className="menu-img"></div>
+            <div className="offer-menu" id="menu-section">
+              <div className="menu-img">
+                <figure><img src={menu}/></figure>
+              </div>
               <h3>The Menu</h3>
               <p>
-                It's the result of days spent shopping at the market,
-                cultivating the land after generations of family heritage, using
-                local ingredients, and following seasonal availability. All of
-                this is showcased in a menu that changes with the seasons.
+                Where we have wanted to capture our tradition, heritage, and
+                mestizaje. The menus are born for you to discover the true
+                essence of Nostre and experience the cuisine that we are.
               </p>
-              <div className="offer-letter-buttons menu-button">
-                <button>View the Menu</button>
-              </div>
+              <ul className="menu-list">
+                <li className="menu-0">
+                  <strong className="menu-title"> Øpportunity Menu:</strong>
+                  <ul>
+                    <li>Aperitif sequence</li>
+                    <li>5 savory courses</li>
+                    <li>1 sweet course</li>
+                  </ul>
+                  <p>Price: €60 per person</p>
+                  <div className="menu-0-buttons">
+                    <button>
+                      <a href={Comida} target="_blank"></a>View menu
+                    </button>
+                    <button>
+                      <a href="/#reservation">Make a reservation</a>
+                    </button>
+                  </div>
+                </li>
+                <li className="menu-1">
+                  <strong className="menu-title"> Ørigin Menu:</strong>
+                  <ul>
+                    <li>Aperitif sequence</li>
+                    <li>6 savory courses</li>
+                    <li>2 sweet courses</li>
+                  </ul>
+                  <p>Price: €75 per person</p>
+                  <div className="menu-1-buttons">
+                    <button>
+                      <a href={Comida} target="_blank"></a>View menu
+                    </button>
+                    <button>
+                      <a href="/#reservation">Make a reservation</a>
+                    </button>
+                  </div>
+                </li>
+              </ul>
+
+              {/*<div className="offer-letter-buttons menu-button">
+    <button id="menu-section" onClick={handleMenu}>
+      View the menu
+    </button>
+  </div>*
+  <div
+    className={`${menuOpen ? "modal-open" : "modal"}`}
+    id="modal-container"
+  >
+    <div className="modal-content" id="modal">
+      <div className="modal-content-title">
+        <h4>Includes:</h4>
+        <button className="modal-button" onClick={closeMenuModal}>
+          Ø
+        </button>
+      </div>
+      <div className="modal-text">
+        <ul>
+          <li>2 Starters</li>
+          <li>Fish, meat, and dessert</li>
+          <li>Water and bread service</li>
+          <li>€37 per person</li>
+        </ul>
+      </div>
+      <p>The menu is prepared according to market products.</p>
+    </div>
+  </div>/*/}
             </div>
           </div>
         </div>
@@ -109,25 +174,74 @@ const Oferta = ({ lenguage }) => {
                   Ver La Carta
                 </a>
               </button>
+              <button>
+                <a href="/#reservation">Hacer una reserva</a>
+              </button>
             </div>
           </div>
           <div className="offer-menu" id="menu-section">
-            <div className="menu-img"></div>
+            <div className="menu-img">
+              <figure><img src={menu}/></figure>
+            </div>
             <h3>El menú</h3>
             <p>
-              Es el resultado de los días de compra en el mercado, el cultivo de
-              la tierra tras la herencia familiar, el kilometro cero y la
-              temporalidad, plasmadas en un menú que varía en consonancia con la
-              época del año
+              Donde hemos querido plasmar nuestra tradición, herencia y
+              mestizaje. Los menús nacen para que descubras la verdadera esencia
+              de Nostre y vivas la experiencia de la cocina que somos.
             </p>
-            <div className="offer-letter-buttons menu-button">
-              <button id="menu-section" onClick={handleMenu}>Ver el menú</button>
-            </div>
-            <div className={`${menuOpen ? "modal-open" : "modal"}`} id="modal-container">
+            <ul className="menu-list">
+              <li className="menu-0">
+                <strong className="menu-title">Menú  Øportunidad:</strong>
+                <ul>
+                  <li>Secuencia de aperitivos</li>
+                  <li>5 pases salados</li>
+                  <li>1 pase dulce</li>
+                </ul>
+                <p>Precio: 60€ por persona</p>
+                <div className="menu-0-buttons">
+                  <button>
+                    <a href={Comida} target="_blank">Ver el menú</a>
+                  </button>
+                  <button>
+                    <a href="/#reservation">Hacer una reserva</a>
+                  </button>
+                </div>
+
+              </li>
+              <li className="menu-1">
+                <strong className="menu-title">Menú  Ørigen:</strong>
+                <ul>
+                  <li>Secuencia de aperitivos</li>
+                  <li>6 pases salados</li>
+                  <li>2 pases dulces</li>
+                </ul>
+                <p>Precio: 75€ por persona</p>
+                <div className="menu-1-buttons">
+                  <button>
+                    <a href={Comida} target="_blank"></a>Ver el menú
+                  </button>
+                  <button>
+                    <a href="/#reservation">Hacer una reserva</a>
+                  </button>
+                </div>
+              </li>
+            </ul>
+
+            {/*<div className="offer-letter-buttons menu-button">
+              <button id="menu-section" onClick={handleMenu}>
+                Ver el menú
+              </button>
+            </div>*
+            <div
+              className={`${menuOpen ? "modal-open" : "modal"}`}
+              id="modal-container"
+            >
               <div className="modal-content" id="modal">
                 <div className="modal-content-title">
                   <h4>Incluye:</h4>
-                  <button className="modal-button"onClick={closeMenuModal}>Ø</button>
+                  <button className="modal-button" onClick={closeMenuModal}>
+                    Ø
+                  </button>
                 </div>
                 <div className="modal-text">
                   <ul>
@@ -139,7 +253,7 @@ const Oferta = ({ lenguage }) => {
                 </div>
                 <p>El menú se elabora según los productos del mercado</p>
               </div>
-            </div>
+            </div>/*/}
           </div>
         </div>
       </div>
