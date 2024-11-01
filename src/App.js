216b960e6e8c189b7e829f0e6d1d820f3941cb9d
FuentesCubells <alejandrodfco@gmail.com>
2024-11-01 16:36:03 +0100

    message

--- src/App.js
+++ src/App.js
@@ -1,21 +1,19 @@
 import React from "react";
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { HashRouter as Router, Routes, Route } from "react-router-dom"; // Cambia BrowserRouter por HashRouter
 import HomeComponent from "./pages/home";
 import RefundPolicy from "./pages/refundPolicy";
 import PrivacyPolicy from "./pages/privacyPolicy";
 
 const App = () => {
-
   return (
-    <>
-      <Router>
-        <Routes>
-          <Route path="/" element={<HomeComponent />} />
-          <Route path="/refund-policy" element={<RefundPolicy />} />
-          <Route path="/privacy-policy" element={<PrivacyPolicy />} />
-        </Routes>
-      </Router>
-    </>
-  )
-}
+    <Router> 
+      <Routes>
+        <Route path="/" element={<HomeComponent />} />
+        <Route path="/refund-policy" element={<RefundPolicy />} />
+        <Route path="/privacy-policy" element={<PrivacyPolicy />} />
+      </Routes>
+    </Router>
+  );
+};
+
 export default App;
